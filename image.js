b985d94b49543e71e0bceb1431c24104c99bea5b
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-04 23:02:36 +0000

    Use a better example to test font width.

--- image.js
+++ image.js
@@ -14,15 +14,15 @@ const height = 600;
 const canvas = Canvas.createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#0e4d92';
+ctx.fillStyle = '#6aadad';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.font = '80px nunito';
+ctx.font = '72px nunito';
 ctx.fillStyle = '#fff';
 
 ctx.textAlign = 'center';
 ctx.textBaseline = 'middle';
-ctx.fillText("It's yale blue", width / 2, height / 2);
+ctx.fillText("It's blue turquoise", width / 2, height / 2);
 
 canvas
   .createPNGStream()
