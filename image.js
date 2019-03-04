7c3dca8ffc2d5312758d3fe782c26a0197f0409e
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-04 22:53:48 +0000

    Add example script to generate image for site.

---
+++ image.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const Canvas = require('canvas');
+
+const fontFile = (name) => {
+  return path.join(__dirname, 'fonts/Nunito', name)
+}
+
+Canvas.registerFont(fontFile('Nunito-Bold.ttf'), { family: 'nunito' });
+
+const width = 800;
+const height = 600;
+
+const canvas = Canvas.createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#0e4d92';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.font = '80px nunito';
+ctx.fillStyle = '#fff';
+
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.fillText("It's yale blue", width / 2, height / 2);
+
+canvas
+  .createPNGStream()
+  .pipe(
+    fs.createWriteStream(
+      path.join(__dirname, 'image.png')
+    )
+  );
