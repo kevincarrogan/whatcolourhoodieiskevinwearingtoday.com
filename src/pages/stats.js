3b1e1090d3d1d50264d4a726b3c2b62ce7e77adb
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-20 13:27:30 +0000

    Add function to get the longest worn colour for stats.

--- src/pages/stats.js
+++ src/pages/stats.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { DateTime } from 'luxon';
 
 import counter from '../utils/counter';
 import Stat from '../components/stat';
@@ -20,13 +21,60 @@ const mostWorn = colours => wornByCountFunc(colours, Math.max);
 
 const leastWorn = colours => wornByCountFunc(colours, Math.min);
 
+const wornDurations = coloursWithDate => {
+  const reversed = coloursWithDate.slice().reverse();
+  let coloursWithDuration = {};
+
+  reversed.forEach(([currentColour, currentTime], i) => {
+    if (i === 0) {
+      return;
+    }
+
+    const [lastColour, lastTime] = reversed[i - 1];
+    const format = 'd LLL yyyy';
+    const currentDateTime = DateTime.fromFormat(currentTime, format);
+    const lastDateTime = DateTime.fromFormat(lastTime, format);
+    let maxDuration = coloursWithDuration[lastColour] || 0;
+    let newDuration = currentDateTime - lastDateTime;
+    if (newDuration > maxDuration) {
+      coloursWithDuration[lastColour] = newDuration;
+    }
+
+    if (i === reversed.length - 1) {
+      let maxDuration = coloursWithDuration[currentColour] || 0;
+      let newDuration = DateTime.fromJSDate(new Date()) - lastDateTime;
+      if (newDuration > maxDuration) {
+        coloursWithDuration[currentColour] = maxDuration;
+      }
+    }
+  });
+
+  return coloursWithDuration;
+};
+
+const longestWorn = colours => {
+  const coloursByDuration = wornDurations(colours);
+  const maxDuration = Math.max(...Object.values(coloursByDuration));
+  const hasMaxDuration = ([k, v]) => v === maxDuration;
+  const filteredColoursByDuration = Object.entries(coloursByDuration).filter(
+    hasMaxDuration
+  );
+  const filteredColours = filteredColoursByDuration.map(d => d[0]);
+
+  return filteredColours;
+};
+
 const StatsPage = ({ data }) => {
   const colours = data.colours.edges.map(item => item.node.colour);
+  const coloursWithDate = data.colours.edges.map(item => [
+    item.node.colour,
+    item.node.date,
+  ]);
 
   return (
     <section>
       <Stat title="Most worn" colours={mostWorn(colours)} />
-      <Stat title="Longest worn" colours={['Red', 'Blue', 'Green']} />
+      <Stat title="Longest worn" colours={longestWorn(coloursWithDate)} />
       <Stat title="Least worn" colours={leastWorn(colours)} />
     </section>
   );
