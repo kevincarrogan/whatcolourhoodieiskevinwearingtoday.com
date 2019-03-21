382f9e35c8b002f18b78b23a90fbd937dc37bf12
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-21 11:23:57 +0000

    Set class name for hovered stat item.

--- src/components/stat.js
+++ src/components/stat.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 import styles from './stat.module.css';
 
-const Stat = ({ title, colours }) => (
-  <article className={styles.stat}>
-    <h1 className={styles.title}>{title}</h1>
-    <ul className={styles.colourList}>
-      {colours.map(hex => (
-        <li
-          className={styles.colourItem}
-          key={hex}
-          style={{ backgroundColor: `#${hex}` }}
-        />
-      ))}
-    </ul>
-  </article>
-);
+const Stat = ({ title, colours }) => {
+  const [selected, setSelected] = useState(undefined);
+  const onMouseOut = evt => {
+    setSelected(undefined);
+  };
+  return (
+    <article className={styles.stat} onMouseOut={onMouseOut}>
+      <h1 className={styles.title}>{title}</h1>
+      <ul className={styles.colourList}>
+        {colours.map(hex => {
+          const className = classNames(styles.colourItem, {
+            [styles.colourItemSelected]: hex === selected,
+          });
+          const onMouseOver = evt => {
+            setSelected(hex);
+          };
+          return (
+            <li
+              className={className}
+              key={hex}
+              style={{ backgroundColor: `#${hex}` }}
+              onMouseOver={onMouseOver}
+            />
+          );
+        })}
+      </ul>
+    </article>
+  );
+};
 
 export default Stat;
