ba3cfb7574c0531cb24bf821f3662d4e3c40e163
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-03-25 17:35:20 +0000

    Move stats function into a separate component.

---
+++ src/components/stats.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { DateTime } from 'luxon';
+
+import counter from '../utils/counter';
+
+import Stat from './stat.js';
+import styles from './stats.module.css';
+
+const wornByCountFunc = (colours, func) => {
+  let colourMap = {};
+  colours.forEach(([name, hex]) => {
+    colourMap[name] = hex;
+  });
+  const names = colours.map(colour => colour[0]);
+  const coloursCount = counter(names);
+  const funcOutput = func(...Object.values(coloursCount));
+  const hasCount = ([k, v]) => v === funcOutput;
+  const filteredColoursCount = Object.entries(coloursCount).filter(hasCount);
+  const filteredColours = filteredColoursCount.map(d => [
+    d[0],
+    colourMap[d[0]],
+  ]);
+
+  return filteredColours;
+};
+
+const mostWorn = colours => wornByCountFunc(colours, Math.max);
+
+const leastWorn = colours => wornByCountFunc(colours, Math.min);
+
+const wornDurations = coloursWithDate => {
+  const reversed = coloursWithDate.slice().reverse();
+  let coloursWithDuration = {};
+
+  reversed.forEach(([currentColour, currentTime], i) => {
+    if (i === 0) {
+      return;
+    }
+
+    const [lastColour, lastTime] = reversed[i - 1];
+    const format = 'd LLL yyyy';
+    const currentDateTime = DateTime.fromFormat(currentTime, format);
+    const lastDateTime = DateTime.fromFormat(lastTime, format);
+    let maxDuration = coloursWithDuration[lastColour] || 0;
+    let newDuration = currentDateTime - lastDateTime;
+    if (newDuration > maxDuration) {
+      coloursWithDuration[lastColour] = newDuration;
+    }
+
+    if (i === reversed.length - 1) {
+      let maxDuration = coloursWithDuration[currentColour] || 0;
+      let newDuration = DateTime.fromJSDate(new Date()) - lastDateTime;
+      if (newDuration > maxDuration) {
+        coloursWithDuration[currentColour] = maxDuration;
+      }
+    }
+  });
+
+  return coloursWithDuration;
+};
+
+const longestWorn = colours => {
+  let colourMap = {};
+  colours.forEach(([name, hex, _]) => {
+    colourMap[name] = hex;
+  });
+  const namesByDuration = colours.map(([name, hex, date]) => [name, date]);
+  const coloursByDuration = wornDurations(namesByDuration);
+  const maxDuration = Math.max(...Object.values(coloursByDuration));
+  const hasMaxDuration = ([k, v]) => v === maxDuration;
+  const filteredColoursByDuration = Object.entries(coloursByDuration).filter(
+    hasMaxDuration
+  );
+  const filteredColours = filteredColoursByDuration.map(d => [
+    d[0],
+    colourMap[d[0]],
+  ]);
+
+  return filteredColours;
+};
+
+const Stats = ({ title, colours, coloursWithDate }) => (
+  <section>
+    <h1 className={styles.statsHeader}>{title}</h1>
+    <div className={styles.statsSection}>
+      <Stat title="Most worn" colours={mostWorn(colours)} />
+      <Stat title="Longest worn" colours={longestWorn(coloursWithDate)} />
+      <Stat title="Least worn" colours={leastWorn(colours)} />
+    </div>
+  </section>
+);
+
+export default Stats;
