3310763a9dfc89681455085f3fcf8ef77875f439
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-05-05 21:56:14 +0100

    Add config and example for CLI

---
+++ update-today.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'ink';
+import SelectInput from 'ink-select-input';
+
+const Demo = () => {
+  const handleSelect = item => {
+    // `item` = { label: 'First', value: 'first' }
+  };
+
+  const items = [
+    {
+      label: 'First',
+      value: 'first',
+    },
+    {
+      label: 'Second',
+      value: 'second',
+    },
+    {
+      label: 'Third',
+      value: 'third',
+    },
+  ];
+
+  return <SelectInput items={items} onSelect={handleSelect} />;
+};
+
+render(<Demo />);
