33d1e032a690ae4f9e20819c111a06cd2611cca9
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-05-06 10:46:56 +0100

    Save current colour when selecting cli item

--- update-today.js
+++ update-today.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { render, AppContext, Color, Box } from 'ink';
 import SelectInput from 'ink-select-input';
+import { DateTime } from 'luxon';
+import fs from 'fs';
+import path from 'path';
+import beautify from 'json-beautify';
 
 import colours from '../data/colours.json';
+
+const setCurrentColour = ({ label, value, exit }) => {
+  const filePath = path.join(__dirname, '..', 'data', 'colours.json');
+  const today = DateTime.fromObject(new Date()).toFormat('yyyy-MM-dd');
+  const colourList = JSON.parse(fs.readFileSync(filePath));
+  colourList.unshift({
+    colour: label,
+    hex: value,
+    date: today,
+  });
+  fs.writeFileSync(filePath, beautify(colourList, null, 2, 100));
+  exit();
+};
 
 const ColourItem = ({ label, value }) => {
   return (
@@ -27,11 +44,16 @@ const Update = ({ exit }) => {
     items.push({
       label: name,
       value: hex,
+      exit,
     });
   });
 
   return (
-    <SelectInput items={items} onSelect={exit} itemComponent={ColourItem} />
+    <SelectInput
+      items={items}
+      onSelect={setCurrentColour}
+      itemComponent={ColourItem}
+    />
   );
 };
 
