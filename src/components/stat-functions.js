40941858a15b9df9746573a9e5de8b8aa414a899
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-05-19 10:33:25 +0100

    Move stat functions into a separate file

---
+++ src/components/stat-functions.js
@@ -0,0 +1,127 @@
+import { DateTime } from "luxon";
+
+import counter from "utils/counter";
+import hexToBrightness from "utils/hex-to-brightness";
+
+const wornByCountFunc = (colours, func) => {
+  let colourMap = {};
+  colours.forEach(([name, hex]) => {
+    colourMap[name] = hex;
+  });
+  const names = colours.map(colour => colour[0]);
+  const coloursCount = counter(names);
+  const funcOutput = func(...Object.values(coloursCount));
+  const hasCount = ([k, v]) => v === funcOutput;
+  const filteredColoursCount = Object.entries(coloursCount).filter(hasCount);
+  const filteredColours = filteredColoursCount.map(d => [
+    d[0],
+    colourMap[d[0]]
+  ]);
+
+  return filteredColours;
+};
+
+export const mostWorn = colours => wornByCountFunc(colours, Math.max);
+
+export const leastWorn = colours => wornByCountFunc(colours, Math.min);
+
+const wornDurations = coloursWithDate => {
+  const reversed = coloursWithDate.slice().reverse();
+  let coloursWithDuration = {};
+
+  reversed.forEach(([currentColour, currentTime], i) => {
+    if (i === 0) {
+      return;
+    }
+
+    const [lastColour, lastTime] = reversed[i - 1];
+    const format = "d LLL yyyy";
+    const currentDateTime = DateTime.fromFormat(currentTime, format);
+    const lastDateTime = DateTime.fromFormat(lastTime, format);
+    let maxDuration = coloursWithDuration[lastColour] || 0;
+    let newDuration = currentDateTime - lastDateTime;
+    if (newDuration > maxDuration) {
+      coloursWithDuration[lastColour] = newDuration;
+    }
+
+    if (i === reversed.length - 1) {
+      let maxDuration = coloursWithDuration[currentColour] || 0;
+      let newDuration = DateTime.fromJSDate(new Date()) - lastDateTime;
+      if (newDuration > maxDuration) {
+        coloursWithDuration[currentColour] = maxDuration;
+      }
+    }
+  });
+
+  return coloursWithDuration;
+};
+
+export const longestWorn = colours => {
+  let colourMap = {};
+  colours.forEach(([name, hex, _]) => {
+    colourMap[name] = hex;
+  });
+  const namesByDuration = colours.map(([name, hex, date]) => [name, date]);
+  const coloursByDuration = wornDurations(namesByDuration);
+  const maxDuration = Math.max(...Object.values(coloursByDuration));
+  const hasMaxDuration = ([k, v]) => v === maxDuration;
+  const filteredColoursByDuration = Object.entries(coloursByDuration).filter(
+    hasMaxDuration
+  );
+  const filteredColours = filteredColoursByDuration.map(d => [
+    d[0],
+    colourMap[d[0]]
+  ]);
+
+  return filteredColours;
+};
+
+const uniqueColours = colours => {
+  let coloursMap = {};
+
+  colours.forEach(([name, hex]) => {
+    coloursMap[name] = hex;
+  });
+
+  return Object.entries(coloursMap);
+};
+
+export const brightest = colours => {
+  const coloursWithBrightness = colours.map(([name, hex]) => [
+    name,
+    hex,
+    hexToBrightness(hex)
+  ]);
+  const brightnessValues = coloursWithBrightness.map(
+    ([name, hex, brightness]) => brightness
+  );
+  const maxBrightness = Math.max(...brightnessValues);
+  const filteredColours = coloursWithBrightness
+    .filter(([name, hex, brightness]) => brightness === maxBrightness)
+    .map(([name, hex, brightness]) => [name, hex]);
+
+  return uniqueColours(filteredColours);
+};
+
+export const darkest = colours => {
+  const coloursWithBrightness = colours.map(([name, hex]) => [
+    name,
+    hex,
+    hexToBrightness(hex)
+  ]);
+  const brightnessValues = coloursWithBrightness.map(
+    ([name, hex, brightness]) => brightness
+  );
+  const minBrightness = Math.min(...brightnessValues);
+  const filteredColours = coloursWithBrightness
+    .filter(([name, hex, brightness]) => brightness === minBrightness)
+    .map(([name, hex, brightness]) => [name, hex]);
+
+  return uniqueColours(filteredColours);
+};
+
+export const allColours = uniqueColours;
+
+export const first = colours => [colours[colours.length - 1]];
+
+export const last = colours => [colours[0]];
