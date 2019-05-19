f5bb823b8006a8a43145f808a29af94b948d6525
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-05-19 10:15:51 +0100

    Remove meta image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
     colour: `dim grey`,
-    hex: `696969`,
+    hex: `696969`
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: path.join(__dirname, `data`),
-      },
+        path: path.join(__dirname, `data`)
+      }
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
     `gatsby-plugin-remove-serviceworker`,
-    `gatsby-plugin-favicon`,
-    `meta-image`,
-  ],
+    `gatsby-plugin-favicon`
+  ]
 };
