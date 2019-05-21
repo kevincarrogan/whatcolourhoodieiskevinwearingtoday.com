93185fa40416989c0e0a3209db6ecba8c4db0797
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-05-21 09:56:45 +0100

    Add test condition to make sure counter handles unsorted arrays

--- src/utils/__tests__/counter.js
+++ src/utils/__tests__/counter.js
@@ -1,12 +1,21 @@
-import counter from '../counter';
+import counter from "../counter";
 
-describe('counter', () => {
-  it('Counts items from an array', () => {
-    const output = counter(['a', 'b', 'b', 'c', 'c', 'c']);
+describe("counter", () => {
+  it("Counts items from an array", () => {
+    const output = counter(["a", "b", "b", "c", "c", "c"]);
     expect(output).toEqual({
       a: 1,
       b: 2,
-      c: 3,
+      c: 3
+    });
+  });
+
+  it("Counts items from an array that is not in order", () => {
+    const output = counter(["b", "c", "a", "b", "c", "c"]);
+    expect(output).toEqual({
+      a: 1,
+      b: 2,
+      c: 3
     });
   });
 });
