55202dd9a140d258a6856aa0dbeff7c77bc4ccfd
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-06-14 21:14:10 +0100

    Change background of side bar based on current item

--- src/components/history-scroll-position.js
+++ src/components/history-scroll-position.js
@@ -1,24 +1,34 @@
 import React from "react";
 import classNames from "classnames";
 
+import isLightColour from "utils/is-light-colour";
+
 import styles from "./history-scroll-position.module.css";
 
-const HistoryScrollPosition = ({ coloursWithDate, selected }) => (
-  <div className={styles.wrapper}>
-    <ol className={styles.list}>
-      {coloursWithDate.map(([hex, name, date], i) => (
-        <li
-          className={classNames(styles.item, {
-            [styles.selected]: i === selected
-          })}
-          key={i}
-          style={{
-            backgroundColor: `#${hex}`
-          }}
-        />
-      ))}
-    </ol>
-  </div>
-);
+const HistoryScrollPosition = ({ coloursWithDate, selected }) => {
+  const [hex] = coloursWithDate[selected];
+
+  const classes = classNames(styles.list, {
+    [styles.isLightColour]: isLightColour(hex)
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      <ol className={classes}>
+        {coloursWithDate.map(([hex, name, date], i) => (
+          <li
+            className={classNames(styles.item, {
+              [styles.selected]: i === selected
+            })}
+            key={i}
+            style={{
+              backgroundColor: `#${hex}`
+            }}
+          />
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 export default HistoryScrollPosition;
